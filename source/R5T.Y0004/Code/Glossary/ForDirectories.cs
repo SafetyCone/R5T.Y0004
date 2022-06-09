f09d7ee4f3f206e3bf62f0b22a05e1b7b1e52af2
davidcoats <david@getsafetycone.com>
2022-06-09 12:43:39 -0700

    Interim changes.

---
+++ source/R5T.Y0004/Code/Glossary/ForDirectories.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace R5T.Y0004
+{
+    public static partial class Glossary
+    {
+        public static class ForDirectories
+        {
+            /// <summary>
+            /// <inheritdoc cref="RepositoryDirectory" path="/definition"/>
+            /// </summary>
+            /// <definition>A repository directory contains the <inheritdoc cref="RepositoryGitDirectory" path="/name"/> directory, as well as all the files in the repository.</definition>
+            /// <name><i>repository directory</i></name>
+            public static readonly object RepositoryDirectory;
+
+            /// <summary>
+            /// <inheritdoc cref="RepositoryGitDirectory" path="/definition"/>
+            /// </summary>
+            /// <definition>Every git repository has a ".git" directory containing all git-related files.</definition>
+            /// <name><i>git directory</i></name>
+            public static readonly object RepositoryGitDirectory;
+        }
+    }
+}
--- source/R5T.Y0004/Code/Interfaces/Interface1.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-
-namespace R5T.Y0004
-{
-    public interface Interface1
-    {
-    }
-}
